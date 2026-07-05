426646b3483cea4b849c795551f6a962e5d3f438
PixelSaver <stephen.k@proton.me>
2026-07-05 15:11:54 -0700

    Added gender selection

---
+++ scenes/gender.gd
@@ -0,0 +1,17 @@
+extends VBoxContainer
+
+@onready var male_text: RichTextLabel = $Male/Numter
+@onready var male_slider: HSlider = $Male/MarginContainer/HSlider
+@onready var female_text: RichTextLabel = $Female/Numter
+@onready var female_slider: HSlider = $Female/MarginContainer/HSlider
+
+func _ready() -> void:
+	male_slider.value_changed.connect(_update_male)
+	female_slider.value_changed.connect(_update_female)
+
+func _update_male(val:float) -> void:
+	male_text.text = "Male: %.1f%%" % val
+	male_text.modulate = lerp(Color.WHITE, Color.BLUE, log(val)/log(100.))
+func _update_female(val:float) -> void:
+	female_text.text = "Female: %.1f%%" % val
+	female_text.modulate = lerp(Color.WHITE, Color.HOT_PINK, log(val)/log(100.))
--- scenes/ssn.gd
+++ scenes/ssn.gd
@@ -1,9 +1,19 @@
 extends HBoxContainer
 
+@onready var numter: RichTextLabel = $Numter
+
 @onready var color: ColorPickerButton = $ColorPickerButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	color.color_changed.connect(_on_color_changed)
 
-func _on_color_changed() -> void: pass
+func _on_color_changed(col:Color) -> void: 
+	var hex_str = col.to_html(true)
+	var num = hex_str.hex_to_int()
+	var last = roundi(num % 10000)
+	num /= 10000
+	var mid = roundi(num % 100)
+	num /= 100
+	var first = roundi(num % 1000)
+	numter.text = "SSN: %03d-%02d-%04d" % [first, mid, last]
